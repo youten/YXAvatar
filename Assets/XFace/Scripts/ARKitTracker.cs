03e94ad79751d0338848322e121bc86b52756d95
youten <youtenredo@gmail.com>
2018-09-23 04:54:05 +0900

    switch Android and update UniVRM to 0.43

--- Assets/XFace/Scripts/ARKitTracker.cs
+++ Assets/XFace/Scripts/ARKitTracker.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 using UnityEngine.XR.iOS;
 
 namespace XFace
